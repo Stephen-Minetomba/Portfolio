dcb554d62251d0de03caba2e371b535799004ab2
Stephen <minetomba@proton.me>
2026-03-05 11:50:14 +0200

    Added some style to the compile.sh script in the rust kernel

--- kernel/kernel/compile.sh
+++ kernel/kernel/compile.sh
@@ -5,6 +5,5 @@ rustup component add rust-src llvm-tools-preview
 cargo build --release --target x86_64-unknown-uefi
 mv target/x86_64-unknown-uefi/release/kernel.efi ./kernel.efi
 rm -rf Cargo.lock target
-cd ../run
-./copy.sh
-cd ../kernel
+mv kernel.efi ../run/EFI/BOOT/BOOTX64.EFI
+echo "🚀 Kernel is ready to run."
--- kernel/run/copy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp ../kernel/kernel.efi ./EFI/BOOT/BOOTX64.EFI
