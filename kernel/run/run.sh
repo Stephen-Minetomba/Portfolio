6111023f61204949470cc9867b8eb543aecc5e0c
Stephen <minetomba@proton.me>
2026-03-06 19:40:06 +0200

    Preparing to build a full kernel (stable kernel release).

--- kernel/run/run.sh
+++ kernel/run/run.sh
@@ -2,6 +2,7 @@
 sudo qemu-system-x86_64 \
     -nodefaults \
     -serial stdio \
+    -vga std \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
     -drive if=pflash,format=raw,file=OVMF_VARS.fd \
     -drive format=raw,file=fat:rw:. \
